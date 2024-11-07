90d82833f3c8672f12ad6d0427015a3b42225084
LukasTopo <lukassantana_rgt@hotmail.com>
2024-11-06 22:37:05 -0300

    sidebar, footer, article

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@
 window.addEventListener("scroll", function() {
     const sideBar = document.getElementById("sideBar");
     const sidebarMenu = document.getElementById("sideBarMenu");
+    const articleWidth = document.getElementsByTagName("article")[0];
     if (window.scrollY > 250 && this.window.scrollY < 700) { 
         sideBar.style.display = "block";
-        sidebarMenu.style.display = "block";
+        sidebarMenu.style.display = "flex";
+        articleWidth.style.width = "85%";
     }
     else { 
         sideBar.style.display = "none";
-        sidebarMenu.style.display = "none"; 
+        sidebarMenu.style.display = "none";
+        articleWidth.style.width = "100%";
     }
 });
